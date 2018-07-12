c715662d9d8de575eb1fd3534402d4f809b8b281
egebilican <egebilican@gmail.com>
2018-07-12 22:04:11 +0300

    cors enabled

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const express = require('express')
 const app = express()
 const routes = require('./routes');
 const bodyParser = require("body-parser");
+var cors = require('cors')
+
 port = process.env.PORT || 3000;
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cors());
 routes(app);
 
 
