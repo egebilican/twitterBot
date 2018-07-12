f943421419743b873e68835dcba87a1d3d5ed472
egebilican <egebilican@gmail.com>
2018-07-12 20:42:04 +0300

    routes

--- app.js
+++ app.js
@@ -1,86 +1,17 @@
-var Twitter = require('twitter');
-var config = require('./config.js');
-var T = new Twitter(config);
 
-// Set up your search parameters
-var params = [
-  {
-    q: '#vegan',
-    count: 10,
-    result_type: 'recent'
-  },
-  {
-    q: '#morissey',
-    count: 10,
-    result_type: 'recent'
-  }
-];
+const express = require('express')
+const app = express()
+const routes = require('./routes');
+const bodyParser = require("body-parser");
+port = process.env.PORT || 3000;
 
-//TODO: duzgun calisiyor mu bak
 
-for (i = 0; i < params.length; i++) {
-  // Initiate your search using the above paramaters
-  T.get('search/tweets', params[i], function(err, data, response) {
-    // If there is no error, proceed
-    if (!err) {
-      // Loop through the returned tweets
-      for (let i = 0; i < data.statuses.length; i++) {
-        // Get the tweet Id from the returned data
-        let id = { id: data.statuses[i].id_str };
-        let text = { status: data.statuses[i].text };
-        console.log(text.status);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-        //retweet(id)
-        //fav(id)
-        post(text.status);
-      }
-    } else {
-      console.log(err);
-    }
-  });
-}
+routes(app);
 
-function retweet(id) {
-  T.post('statuses/retweet', { id: id.id }, function(err, resp) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('retweeted', id);
-    }
-  });
-}
 
-function fav(id) {
-  T.post('favorites/create', id, function(err, response) {
-    // If the favorite fails, log the error message
-    if (err) {
-      console.log(err[0].message);
-    }
-    // If the favorite is successful, log the url of the tweet
-    else {
-      let username = response.user.screen_name;
-      let tweetId = response.id_str;
-      console.log(
-        'Favorited: ',
-        `https://twitter.com/${username}/status/${tweetId}`
-      );
-    }
-  });
-}
+app.listen(port, () => console.log('Example app listening on port', port))
 
-function post(text) {
-  T.post('statuses/update', { status: text }, function(err, resp) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('posted: ', text);
-    }
-  });
-}
 
-// const express = require('express')
-// const app = express()
-
-// app.get('/', (req, res) => res.send('Hello World!'))
-
-// app.listen(3000, () => console.log('Example app listening on port 3000!'))
---
+++ routes.js
@@ -0,0 +1,113 @@
+const Twitter = require('twitter');
+const config = require('./config.js');
+const T = new Twitter(config);
+
+
+function routes(app) {
+  app.get("/", function(req, res) {
+    res.status(200).send("kanalima hosgeldiniz");
+  });
+
+  app.get('/search/:searchTerm', (req, res) => {
+    searchTweet(req.params.searchTerm, () =>
+      res.status(200).send('search ' + req.params.searchTerm)
+    );
+  });
+
+  app.get('/retweet/:retweetTerm', (req, res) => {
+    retweetTweet(req.params.retweetTerm, () =>
+      res.status(200).send('retweeted ' + req.params.retweetTerm)
+    );
+  });
+}
+
+
+
+function searchTweet(query, callback) {
+  T.get('search/tweets', {q: '#'+query, count:1}, function(err, data, response) {
+    // If there is no error, proceed
+    console.log('FETCHING  FOR : ', query);
+    if (!err) {
+      // Loop through the returned tweets
+      for (let j = 0; j < data.statuses.length; j++) {
+        // Get the tweet Id from the returned data
+        let id = { id: data.statuses[j].id_str };
+        let text = { status: data.statuses[j].text };
+        console.log('RESULTS FOR : ', query);
+        console.log(j, text.status);
+
+        //retweet(id);
+        //fav(id)
+        //post(text.status);
+      }
+    } else {
+      console.log(err);
+    }
+  });  
+  callback();
+}
+
+function retweetTweet(query, callback) {
+T.get('search/tweets', {q: '#'+query, count:1}, function(err, data, response) {
+  // If there is no error, proceed
+  console.log('FETCHING  FOR : ', query);
+  if (!err) {
+    // Loop through the returned tweets
+    for (let j = 0; j < data.statuses.length; j++) {
+      // Get the tweet Id from the returned data
+      let id = { id: data.statuses[j].id_str };
+      let text = { status: data.statuses[j].text };
+      console.log('RESULTS FOR : ', query);
+      console.log(j, text.status);
+
+      retweet(id);
+      //fav(id)
+      //post(text.status);
+    }
+  } else {
+    console.log(err);
+  }
+});  
+callback();
+}
+
+function retweet(id) {
+T.post('statuses/retweet', { id: id.id }, function(err, resp) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('retweeted', id);
+  }
+});
+}
+
+function fav(id) {
+T.post('favorites/create', id, function(err, response) {
+  // If the favorite fails, log the error message
+  if (err) {
+    console.log(err[0].message);
+  }
+  // If the favorite is successful, log the url of the tweet
+  else {
+    let username = response.user.screen_name;
+    let tweetId = response.id_str;
+    console.log(
+      'Favorited: ',
+      `https://twitter.com/${username}/status/${tweetId}`
+    );
+  }
+});
+}
+
+function post(text) {
+T.post('statuses/update', { status: text }, function(err, resp) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('posted: ', text);
+  }
+});
+}
+
+
+module.exports = routes;
