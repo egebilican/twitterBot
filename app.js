86d344c32e8a4e50792113546987ed84b490e1a6
egebilican <egebilican@gmail.com>
2018-07-12 22:15:22 +0300

    corss

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const routes = require('./routes');
-const bodyParser = require("body-parser");
-var cors = require('cors')
+const bodyParser = require('body-parser');
 
 port = process.env.PORT || 3000;
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 routes(app);
 
-
-app.listen(port, () => console.log('Example app listening on port', port))
-
-
+app.listen(port, () => console.log('Example app listening on port', port));
