63d68710032a1fb513bc4dc93bfe974dc040e3a5
egebilican <egebilican@gmail.com>
2018-07-14 16:31:06 +0300

    cors again

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const routes = require('./routes');
 const bodyParser = require('body-parser');
 
 port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+routes(app);
+
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-routes(app);
 
 app.listen(port, () => console.log('Example app listening on port', port));
