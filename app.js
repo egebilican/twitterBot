1e16dfc26c4ed7af127cfd5a5bc9976ff55655b9
egebilican <egebilican@gmail.com>
2018-07-14 16:33:08 +0300

    cors test

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ const bodyParser = require('body-parser');
 
 port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-routes(app);
+// routes(app);
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
+});
+
+app.get("/", function(req, res) {
+  res.status(200).send("kanalima hosgeldiniz");
 });
 
 app.listen(port, () => console.log('Example app listening on port', port));
