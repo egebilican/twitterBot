76d3387c28511d27325db98da1c8d86fd9d6a0ed
egebilican <egebilican@gmail.com>
2018-07-14 16:25:16 +0300

    still cors

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ const bodyParser = require('body-parser');
 port = process.env.PORT || 3000;
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 app.use(bodyParser.json());
